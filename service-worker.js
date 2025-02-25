18fd78692ad2b53c1b0bdd7832747046eed1888d
Sethjesus <162777479+Sethjesus@users.noreply.github.com>
2025-02-25 11:08:22 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
-self.addEventListener("install", function(event) {
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("pwa-cache").then(function(cache) {
+        caches.open("pwa-cache").then((cache) => {
             return cache.addAll([
                 "index.html",
-                "manifest.json"
+                "manifest.json",
+                "icon-192.png",
+                "icon-512.png"
             ]);
         })
     );
     console.log("✅ Service Worker 安裝完成");
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
 });
+
